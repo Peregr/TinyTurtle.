76601cbb5481562b1998cc98f88aa489fc5e2c87
Anahi Peregrina <aperegrina.student@hsfi.us>
2017-01-19 21:49:28 +0000

    tinyturtle

---
+++ script.js
@@ -0,0 +1,14 @@
+TinyTurtle.apply(window); 
+forward(30);
+right(90);
+forward(60);
+right(90);
+forward(30);
+right(90);
+forward(60);
+right(90);
+forward(30);
+right(45);
+forward(40);
+right(85);
+forward(45);
